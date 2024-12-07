75bb0872272c0e2f962aaa7ec6edd2fc830455af
4mritGiri <amritgiri02595@gmail.com>
2024-12-07 07:05:27 +0545

    Fixed: user view

--- shop/models.py
+++ shop/models.py
@@ -113,7 +113,7 @@ class User(AbstractUser):
         verbose_name_plural = 'Users'
 
     def __str__(self):
-        return self.email
+        return self.username if self.username else self.email
 
 
 
